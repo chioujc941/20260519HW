af51c5f841c8247e3a7ca18ac20e64aaeea9b001
chioujc941 <chioujc941@gmail.com>
2026-05-19 19:35:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import streamlit as st
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
-    st.write("###  行事曆群組")
+    st.write("Jessie 行事曆網頁")
     st.radio("選擇群組", ["工作", "家庭", "朋友"])
 
-col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
+col_left, col_right = st.columns([1, 3], gap="large")
+
+with col_left:
+    st.write("###  新增區") 
+    st.button("按鈕放左邊")
+    with st.container(border=True): 
+        st.write(" 標題：開學典禮") 
+        st.write(" 時間：09:00")
+
+with col_right: 
+    st.write("###  設定區") 
+    st.button("控制項放右邊")
+    tab1, tab2, tab3 = st.tabs(["首頁", "圖表", "設定"])
+    with tab1: 
+        st.header("首頁") 
+        st.write("這是首頁內容")
+    with tab2: 
+        st.header("圖表") 
+        st.line_chart([1, 5, 2, 6, 2, 1])
+    with tab3: 
+        st.header("設定") 
+        name = st.text_input("你的名字") 
+        st.write(f"Hello {name}")
