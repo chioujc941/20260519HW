9b8a42c8332b9a43fec1f9d5c24dd4da007a595d
chioujc941 <chioujc941@gmail.com>
2026-05-19 20:26:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,22 @@ with col_left:
                      "挑選辨識顏色",
                      "#1A73E8"
                     )
+        view = st.segmented_control(
+              "檢視模式",
+              ["月視角", "週視角"],
+              default="月視角"
+            )
+    
+        tag = st.pills(
+              "行程屬性",
+              ["#工作", "#家庭", "#緊急"]
+            )
+    
+        mins = st.number_input(
+             "行程開始前幾分鐘提醒？",
+             min_value=0, max_value=60,
+             value=15
+            )
         
 
 with col_right: 
@@ -40,24 +56,6 @@ with col_right:
     with tab2: 
         st.header("已完成事項") 
         st.line_chart([1, 5, 2, 6, 2, 1])
-
-    view = st.segmented_control(
-              "檢視模式",
-              ["月視角", "週視角"],
-              default="月視角"
-            )
-    
-    tag = st.pills(
-              "行程屬性",
-              ["#工作", "#家庭", "#緊急"]
-            )
-    
-    mins = st.number_input(
-             "行程開始前幾分鐘提醒？",
-             min_value=0, max_value=60,
-             value=15
-            )
-
 
 st.write("上面是大標題")
 st.divider()
